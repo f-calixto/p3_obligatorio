c5df848b32615651b96713ed35f8099add8ece67
coding-kiko <kikoaudi2001@gmail.com>
2026-07-02 23:45:32 -0300

    TAD previaturas

---
+++ previaturas.h
@@ -0,0 +1,28 @@
+#ifndef PREVIATURAS_H
+#define PREVIATURAS_H
+
+#include "boolean.h"
+
+// ================== Previaturas (Grafo) =================
+
+const int N = 30;
+
+typedef boolean Grafo[N][N];
+
+// ================= PRIMITIVAS ==================
+void crear(Grafo& g);
+boolean perteneceVertice(Grafo g, int u);
+boolean perteneceArista(Grafo g, int u, int v);
+// Precondición: el vértice no pertenece al grafo.
+void insertarVertice(Grafo& g, int u);
+// Precondición: la arista no pertenece al grafo
+void insertarArista(Grafo& g, int u, int v);
+// Precondición: el vértice pertenece al grafo.
+void gradoVertice(Grafo g, int u);
+
+// ================= EXTENSIONES =================
+// Precondición: v y u pertenencen al grafo
+boolean hayCamino(Grafo g, int v, int u);
+void obtenerPrevias(Grafo g, int u, int res[], int& cant);
+
+#endif
