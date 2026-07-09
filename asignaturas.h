fcf66cd499435bd01cec584b93a0d6d0d0603fb0
Lucas Cuevas <lucascuevas54902533@gmail.com>
2026-07-09 14:09:17 -0300

    Update asignaturas.h

--- asignaturas.h
+++ asignaturas.h
@@ -16,19 +16,19 @@ typedef Celda Asignaturas[MAX_ASIG];
 
 // ================= PRIMITIVAS ==================
 // Inicializa el diccionario poniendo todas las celdas en FALSE
-void make(Asignaturas& A);
+void make(Asignaturas &A);
 
 // Devuelve TRUE si la asignatura existe en esa posición del diccionario arreglo A
 boolean member(Asignaturas A, int numAsig);
 
 // Precondición: la asignatura a insertar no existe en diccionario A
-void insert(Asignaturas& A, Asignatura a);
+void insert(Asignaturas &A, Asignatura a);
 
 // Precondición: la asignatura existe en el diccionario A
 Asignatura find(Asignaturas A, int numAsig);
 
 // Precondición: la asigantura a sustituir existe en diccionario A
-void modify(Asignaturas& A, Asignatura a, int numAsig);
+void modify(Asignaturas &A, Asignatura a, int numAsig);
 
 // Precondición: la asignatura existe en diccionario A
 void delete_(Asignaturas& A, int numAsig);
