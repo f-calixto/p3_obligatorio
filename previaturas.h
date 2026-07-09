e9adb228be36a1d14652dd5f5acb2c55a3c0b927
Lucas Cuevas <lucascuevas54902533@gmail.com>
2026-07-09 04:24:31 -0300

    Update previaturas.h

--- previaturas.h
+++ previaturas.h
@@ -1,3 +1,23 @@
+/* ==================================================================================
+MÓDULO: PREVIATURAS (GRAFO)
+==================================================================================
+DESCRIPCIÓN CONCEPTUAL:
+Representa la relacion entre asignaturas de la carrera mediante un Grafo Dirigido y Acíclico
+implementado sobre una Matriz de Adyacencia de booleanos de tamaño fijo (30x30).
+
+RELACIÓN CON LA REALIDAD Y LA LETRA DEL OBLIGATORIO:
+- VÉRTICES: Representan los números enteros únicos de las Asignaturas.
+- ARISTAS: Representan las relaciones de previatura entre dos materias.
+  Si la asignatura 'u' es previa de 'v', la celda g[u][v] será TRUE.
+
+  La operación 'hayCamino' evalúa mediante el algoritmo
+   DFS si existe una correlatividad directa o transitiva entre dos asignaturas,
+   permitiendo validar si un estudiante cumple con los requisitos del plan de
+   estudios antes de registrar una nueva nota en su Escolaridad.
+==================================================================================
+*/
+
+
 #ifndef PREVIATURAS_H
 #define PREVIATURAS_H
 
@@ -10,19 +30,27 @@ const int N = 30;
 typedef boolean Grafo[N][N];
 
 // ================= PRIMITIVAS ==================
+
 void crear(Grafo& g);
+
 boolean perteneceVertice(Grafo g, int u);
+
 boolean perteneceArista(Grafo g, int u, int v);
-// Precondición: el vértice no pertenece al grafo.
-void insertarVertice(Grafo& g, int u);
+
 // Precondición: la arista no pertenece al grafo
 void insertarArista(Grafo& g, int u, int v);
+
 // Precondición: el vértice pertenece al grafo.
 void gradoVertice(Grafo g, int u);
 
+
+
 // ================= EXTENSIONES =================
-// Precondición: v y u pertenencen al grafo
+
+// Precondición: v y u pertenecen al grafo.
+// determina si v es olbigatoria para cursar u
 boolean hayCamino(Grafo g, int v, int u);
+
 // Precondición: u pertenence al grafo
 void listarPreviaturas(Grafo g, int u);
 // Precondición: u pertenence al grafo
