d26f8fa7160a7ed791c82bec919fbf27763862d1
Lucas Cuevas <lucascuevas54902533@gmail.com>
2026-07-10 18:18:54 -0300

    implementacion de cabezales

--- boolean.cpp
+++ boolean.cpp
@@ -1,1 +1,23 @@
 #include "boolean.h"
+
+void cargarBoolean(boolean& b) 
+{
+    int valor;
+    printf("Ingrese 0 para FALSE o 1 para TRUE: ");
+    scanf("%d", &valor);
+
+    if (valor == 1) {
+        b = TRUE;
+    } else {
+        b = FALSE;
+    }
+}
+
+void mostrarBoolean(boolean b)
+{
+    if (b == TRUE) {
+        printf("TRUE");
+    } else {
+        printf("FALSE");
+    }
+}
