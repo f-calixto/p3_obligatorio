9622b7b1255414757cf8e6e59f08d6f614d46d6b
Lucas Cuevas <lucascuevas54902533@gmail.com>
2026-07-13 15:25:47 -0300

    Update asignaturas.h

--- asignaturas.h
+++ asignaturas.h
@@ -2,36 +2,35 @@
 #define ASIGNATURAS_H
 
 #include "asignatura.h"
+#include "boolean.h"
 
-// ==================== Asignaturas (Diccionario) =================
+// ==================== Asignaturas (Arreglo con Tope, act) =================
 
 const int MAX_ASIG = 30;
 
 typedef struct {
-    boolean existe;
-    Asignatura info;
-} Celda;
-
-typedef Celda Asignaturas[MAX_ASIG];
+    Asignatura act[MAX_ASIG];
+    int tope;
+} Asignaturas;
 
 // ================= PRIMITIVAS ==================
-// Inicializa el diccionario poniendo todas las celdas en FALSE
+// Inicializa el diccionario poniendo el tope en 0
 void make(Asignaturas &A);
 
-// Devuelve TRUE si la asignatura existe en esa posición del diccionario arreglo A
+// Devuelve TRUE si la asignatura existe recorriendo el arreglo hasta el tope
 boolean member(Asignaturas A, int numAsig);
 
-// Precondición: la asignatura a insertar no existe en diccionario A
+// Precondición: la asignatura a insertar no existe en diccionario A y tope < MAX_ASIG
 void insert(Asignaturas &A, Asignatura a);
 
 // Precondición: la asignatura existe en el diccionario A
 Asignatura find(Asignaturas A, int numAsig);
 
-// Precondición: la asigantura a sustituir existe en diccionario A
+// Precondición: la asignatura a sustituir existe en diccionario A
 void modify(Asignaturas &A, Asignatura a, int numAsig);
 
 // Precondición: la asignatura existe en diccionario A
-void delete_(Asignaturas& A, int numAsig);
+void delete_(Asignaturas &A, int numAsig);
 
 // ================= EXTENSIONES =================
 void listar(Asignaturas A);
